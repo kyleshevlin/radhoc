50a0c1848a70fd2ae946b57a03e60572e65937eb
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-04-08 11:30:26 -0700

    add some more tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,3 +10,30 @@ describe('setActionType', () => {
     expect(type).toEqual('RADHOC_FOO')
   })
 })
+
+describe('radhoc', () => {
+  it('will return and call the reducer if action.type is not the right type', () => {
+    const mockFn = jest.fn()
+    const state = {}
+    const action = { type: 'TEST' }
+
+    radhoc(mockFn)(state, action)
+
+    expect(mockFn).toHaveBeenCalled()
+    expect(mockFn).toHaveBeenCalledWith(state, action)
+  })
+
+  it('will return an object if type matches', () => {
+    const mockFn = jest.fn()
+    const state = {}
+    const action = {
+      type: 'RADHOC',
+      update: { test: 'radhoc' }
+    }
+    const expectedState = { test: 'radhoc' }
+
+    const newState = radhoc(mockFn)(state, action)
+
+    expect(newState).toEqual(expectedState)
+  })
+})
